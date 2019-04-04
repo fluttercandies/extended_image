514ad1464982699c791edead7fc5bc62472ed698
zhoumaotuo <zmtzawqlp@live.com>
2019-04-04 22:52:18 +0800

    update project

--- lib/extended_image.dart
+++ lib/extended_image.dart
@@ -1,13 +1,17 @@
 library extended_image;
 
-export 'src/border_painter.dart';
-export 'src/extended_network_image_utils.dart';
-export 'src/extended_network_image_provider.dart';
+export 'src/gesture/extended_image_gesture.dart';
+export 'src/gesture/extended_image_gesture_page_view.dart';
+export 'src/gesture/extended_image_gesture_utils.dart';
+
+export 'src/image/extended_raw_image.dart';
+export 'src/image/extended_render_image.dart';
+
+export 'src/network/extended_network_image_utils.dart';
+export 'src/network/extended_network_image_provider.dart';
+
 export 'src/extended_image.dart';
-export 'src/extended_raw_image.dart';
-export 'src/extended_render_image.dart';
-export 'src/extended_image_utils.dart';
-export 'src/extended_image_gesture.dart';
-export 'src/extended_image_gesture_page_view.dart';
+export 'src/extended_image_border_painter.dart';
+
 export 'src/extended_image_typedef.dart';
-export 'src/extended_image_gesture_utils.dart';
+export 'src/extended_image_utils.dart';
