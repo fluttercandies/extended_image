83701ed3b9bbe1bedbc7776e473df0d2e93b79a2
wtfan <fanwt883188@gmail.com>
2020-01-09 22:44:44 +0800

    feat(slide)：Customize the offset when sliding. e.g: just like the iOS Album or WeChat, it only closes the picture when you slide it down at the top of it, when you slide it up at the end of the pic, you don't need to move its dx.

--- lib/src/gesture/extended_image_slide_page.dart
+++ lib/src/gesture/extended_image_slide_page.dart
@@ -25,6 +25,9 @@ class ExtendedImageSlidePage extends StatefulWidget {
   ///builder of page background when slide page
   final SlideScaleHandler slideScaleHandler;
 
+  ///change offset when slide page
+  final SlideOffsetHanlder slideOffsetHandler;
+
   ///call back of slide end
   ///decide whether pop page
   final SlideEndHandler slideEndHandler;
@@ -133,6 +136,7 @@ class ExtendedImageSlidePageState extends State<ExtendedImageSlidePage>
     } else if (widget.slideAxis == SlideAxis.vertical) {
       _offset = Offset(0.0, value.dy);
     }
+    _offset = widget.slideOffsetHandler?.call(_offset) ?? _offset;
 
     _scale = widget.slideScaleHandler?.call(_offset) ??
         defaultSlideScaleHandler(
