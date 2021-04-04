19f3ca693ce38a5c580348a8051b218565ca142d
zmtzawqlp <zmtzawqlp@live.com>
2021-04-04 16:41:11 +0800

    add CircleEditorCropLayerPainter demo

--- example/lib/pages/complex/image_editor_demo.dart
+++ example/lib/pages/complex/image_editor_demo.dart
@@ -254,11 +254,30 @@ class _ImageEditorDemoState extends State<ImageEditorDemo> {
                         ),
                         value: const CustomEditorCropLayerPainter(),
                       ),
+                      const PopupMenuDivider(),
+                      PopupMenuItem<EditorCropLayerPainter>(
+                        child: Row(
+                          children: const <Widget>[
+                            Icon(
+                              CupertinoIcons.circle,
+                              color: Colors.blue,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text('Circle'),
+                          ],
+                        ),
+                        value: const CircleEditorCropLayerPainter(),
+                      ),
                     ];
                   },
                   onSelected: (EditorCropLayerPainter value) {
                     if (_cropLayerPainter != value) {
                       setState(() {
+                        if (value is CircleEditorCropLayerPainter) {
+                          _aspectRatio = _aspectRatios[2];
+                        }
                         _cropLayerPainter = value;
                       });
                     }
@@ -466,5 +485,44 @@ class CustomEditorCropLayerPainter extends EditorCropLayerPainter {
     canvas.drawCircle(Offset(cropRect.right, cropRect.top), radius, paint);
     canvas.drawCircle(Offset(cropRect.left, cropRect.bottom), radius, paint);
     canvas.drawCircle(Offset(cropRect.right, cropRect.bottom), radius, paint);
+  }
+}
+
+class CircleEditorCropLayerPainter extends EditorCropLayerPainter {
+  const CircleEditorCropLayerPainter();
+
+  @override
+  void paintCorners(
+      Canvas canvas, Size size, ExtendedImageCropLayerPainter painter) {
+    // do nothing
+  }
+
+  @override
+  void paintMask(
+      Canvas canvas, Size size, ExtendedImageCropLayerPainter painter) {
+    final Rect rect = Offset.zero & size;
+    final Rect cropRect = painter.cropRect;
+    final Color maskColor = painter.maskColor;
+    canvas.saveLayer(rect, Paint());
+    canvas.drawRect(
+        rect,
+        Paint()
+          ..style = PaintingStyle.fill
+          ..color = maskColor);
+    canvas.drawCircle(cropRect.center, cropRect.width / 2.0,
+        Paint()..blendMode = BlendMode.clear);
+    canvas.restore();
+  }
+
+  @override
+  void paintLines(
+      Canvas canvas, Size size, ExtendedImageCropLayerPainter painter) {
+    final Rect cropRect = painter.cropRect;
+    if (painter.pointerDown) {
+      canvas.save();
+      canvas.clipPath(Path()..addOval(cropRect));
+      super.paintLines(canvas, size, painter);
+      canvas.restore();
+    }
   }
 }
