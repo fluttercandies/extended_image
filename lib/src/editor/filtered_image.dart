112cc8be0c2f07129edd7575003a3f9ddb62ae3f
vishnukvmd <vishnu@ente.io>
2021-09-25 13:56:24 +0530

    Use matrices from image_editor for consistency

--- lib/src/editor/filtered_image.dart
+++ lib/src/editor/filtered_image.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/widgets.dart';
 import 'dart:math';
+import 'package:flutter/widgets.dart';
+import 'package:image_editor/image_editor.dart';
 
 class FilteredImage extends StatelessWidget {
   const FilteredImage({
@@ -95,113 +96,10 @@ class ColorFilterGenerator {
   }
 
   static List<double> brightnessAdjustMatrix({double value = 1}) {
-    if (value <= 0)
-      value = value * 255;
-    else
-      value = value * 100;
-
-    if (value == 0)
-      return [
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-      ];
-
-    return List<double>.from(<double>[
-      1,
-      0,
-      0,
-      0,
-      value,
-      0,
-      1,
-      0,
-      0,
-      value,
-      0,
-      0,
-      1,
-      0,
-      value,
-      0,
-      0,
-      0,
-      1,
-      0
-    ]).map((i) => i.toDouble()).toList();
+    return ColorOption.brightness(value).matrix;
   }
 
   static List<double> saturationAdjustMatrix({double value = 1}) {
-    value = value * 100;
-
-    if (value == 0)
-      return [
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-        0,
-        0,
-        0,
-        0,
-        1,
-        0,
-      ];
-
-    double x =
-        ((1 + ((value > 0) ? ((3 * value) / 100) : (value / 100)))).toDouble();
-    double lumR = 0.3086;
-    double lumG = 0.6094;
-    double lumB = 0.082;
-
-    return List<double>.from(<double>[
-      (lumR * (1 - x)) + x,
-      lumG * (1 - x),
-      lumB * (1 - x),
-      0,
-      0,
-      lumR * (1 - x),
-      (lumG * (1 - x)) + x,
-      lumB * (1 - x),
-      0,
-      0,
-      lumR * (1 - x),
-      lumG * (1 - x),
-      (lumB * (1 - x)) + x,
-      0,
-      0,
-      0,
-      0,
-      0,
-      1,
-      0,
-    ]).map((i) => i.toDouble()).toList();
+    return ColorOption.saturation(value).matrix;
   }
 }
