a35c4c58748d01d4dfb351f32160a9f8a361a9f2
Loïc <49951010+lolocomotive@users.noreply.github.com>
2025-04-07 03:00:49 +0200

    Use before/afterPaintImage callbacks in editing too (#662)
    * Use before/afterPaintImage callbacks in editing too

--- lib/src/editor/editor.dart
+++ lib/src/editor/editor.dart
@@ -198,6 +198,7 @@ class ExtendedImageEditorState extends State<ExtendedImageEditor>
       alignment: extendedImage.alignment,
       repeat: extendedImage.repeat,
       centerSlice: extendedImage.centerSlice,
+      beforePaintImage: extendedImage.beforePaintImage,
       //matchTextDirection: extendedImage.matchTextDirection,
       //don't support TextDirection for editor
       matchTextDirection: false,
@@ -205,6 +206,7 @@ class ExtendedImageEditorState extends State<ExtendedImageEditor>
       filterQuality: extendedImage.filterQuality,
       editActionDetails: _editActionDetails,
       afterPaintImage: (Canvas canvas, Rect rect, ui.Image image, Paint paint) {
+        extendedImage.afterPaintImage?.call(canvas, rect, image, paint);
         if (_history.isEmpty) {
           _currentIndex = -1;
           _saveCurrentState();
